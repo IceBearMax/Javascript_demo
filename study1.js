e43630c48a589406e67e895c05edfe853e60ab27
IceBearMax <1034089857@qq.com>
2024-01-26 13:56:17 +0800

    study1 第一次提交

---
+++ study1.js
@@ -0,0 +1,121 @@
+'use strict';
+// let markMass =78;
+// let markHeight =1.69;
+//
+// let johnMass =95;
+// let johnHeight = 1.88;
+//
+// let IBMMark=markMass/(markHeight*markHeight);
+//
+// let IBMJohn = johnMass/(johnHeight*johnHeight);
+//
+// let boo =IBMMark>IBMJohn;
+//
+// console.log(boo);
+// console.log(IBMJohn);
+// console.log(IBMMark);
+// const Tom =18;
+//
+// if (Tom>=18){
+//     console.log(`Tom can drive the car  ,He is ${Tom} year!`);
+// }else{
+//     console.log(`Tom can't do that!`);
+// }
+
+// const MarkMass =78;
+// const MarkHeight=1.76;
+//
+// const JohnMass=95;
+// const JohnHeight =1.88;
+//
+// let MarkBMI =MarkMass/(MarkHeight**2);
+// let JohnBMI=JohnMass/(JohnHeight**2);
+//
+// if(MarkBMI>JohnBMI){
+//     console.log(`Mark's BMI is higher than John's BMI`);
+//
+// }else{
+//     console.log(`John's BMI is higher than Mark's BMI`);
+// }
+//
+// console.log(`Mark's BMI (${MarkBMI}),John 's BMI (${JohnBMI})`);
+
+// let tip;
+// let bill=275;
+// tip=bill;
+//
+// bill>=50&&bill <=300? console.log(`The payment is ${bill+tip*0.15}`):console.log(`The payment is ${bill+tip*0.3}`);
+// function calcAverage(score1,score2,score3){
+//     return (score1+score2+score3)/3;
+// }
+//
+// const dolphinsScore=calcAverage(44,23,71);
+// const koalasScore=calcAverage(85,54,41);
+
+// function checkWinner(dolphinsScore,koalasScore){
+//     const outstand= dolphinsScore-koalasScore;
+//    if(outstand>0){
+//        console.log(`Dolphins win (${dolphinsScore} vs ${koalasScore})`);
+//    }else if(outstand===0){
+//        console.log(`All win!`);
+//    }else{
+//        console.log(`Koalas win (${koalasScore} vs ${dolphinsScore})`);
+//    }
+// }
+//
+// checkWinner(dolphinsScore,koalasScore);
+
+// const friend =['Tom','Jerry','Jay'];
+// friend.push('Steven');
+// friend.unshift('Jerry');
+// console.log(friend);
+// const tim=friend.shift();
+// console.log(tim);
+// console.log(friend);
+
+// const jonas={
+//     firstName:'Jonas',
+//     lastName:'Schmedtman',
+//     age:2037-1991,
+//     job:'teacher',
+//     friends:['Michael','Peter','Steven'],
+//     calcAge:function(){
+//         console.log(this);
+//     }
+// };
+
+
+//const prop =prompt('what information do you what know ?');
+// console.log(jonas[prop]);
+// console.log(jonas.prop);
+// jonas.location ='potuyal';
+// jonas['y']='jdiaj';
+// console.log(jonas.calcAge());
+
+// const Marks= {
+//     fullName:'Marks Miller',
+//     mass:78,
+//     height:1.69,
+//     calacBMI:function(){
+//         return this.mass/this.height**2;
+//     },
+//     BMI:this.calacBMI
+// }
+// const John= {
+//     fullName:'John Simth',
+//     mass:92,
+//     height:1.95,
+//     calacBMI:function(){
+//         this.BMI= this.mass/this.height**2;
+//     }
+//
+//
+// }
+//
+//
+// console.log(`${Marks.fullName} 's BMI is ${Marks.calacBMI()}`);
+// console.log(`${John.fullName} 's BMI is ${John.calacBMI()}`);
+// console.log(John);
+
+//Marks.BMI>John.BMI ?console.log(`Marks's BMI(${Marks.BMI}) is higher that John(${John.BMI})`):console.log(`Marks's BMI(${Marks.BMI}) is lower that John(${John.BMI})`)
+
